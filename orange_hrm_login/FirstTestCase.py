5b5d197b2c16294adc251cd0fd734d36ce7de4f5
sudharaD <72960779+sudharaD@users.noreply.github.com>
2023-12-18 22:53:43 +0530

    creating login code

--- orange_hrm_login/FirstTestCase.py
+++ orange_hrm_login/FirstTestCase.py
@@ -9,3 +9,22 @@
 # #) Verify title of the page: OrangeHRM (Expected).
 # 8) close browser
 
+from selenium import webdriver
+
+# Specify the path to the chromedriver executable
+chromedriver_path = r'D:\NEW_W_ORDER\projects\Automation_with_python\automation_001\drivers\chromedriver-win64\chromedriver.exe'
+
+# Create ChromeOptions instance
+chrome_options = webdriver.ChromeOptions()
+
+# Set any desired options here, if needed
+# chrome_options.add_argument('--headless')
+
+# Specify the path to the chromedriver executable directly in the options
+chrome_options.binary_location = chromedriver_path
+
+# Create the WebDriver instance with the specified options
+driver = webdriver.Chrome(chrome_options)
+
+
+driver.get('https://opensource-demo.orangehrmlive.com/')
