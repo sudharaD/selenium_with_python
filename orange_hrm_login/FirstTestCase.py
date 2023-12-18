03226ff36bfd5a01fb62fb1a32df45700a2d08af
sudharaD <72960779+sudharaD@users.noreply.github.com>
2023-12-18 22:31:00 +0530

    Initial Commit

---
+++ orange_hrm_login/FirstTestCase.py
@@ -0,0 +1,11 @@
+# test Case
+
+# 1) Open Web Browser (Chrome/firefox/Edge).
+# 2) Open URL https://opensource-demo.orangehrmlive.com/
+# 3) Enter username (Admin).
+# 4) Enter password (admin123).
+# 5) Click on Login.
+# 6) Capture title of the home page. (Actual title)
+# #) Verify title of the page: OrangeHRM (Expected).
+# 8) close browser
+
